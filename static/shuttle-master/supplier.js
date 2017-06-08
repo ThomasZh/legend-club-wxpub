eaad1cc9176f121cb121d1dd6317607476363584
zhouchao123 <zc2473@163.com>
2017-06-08 18:33:55 +0800

    列表页加载更多

--- static/shuttle-master/supplier.js
+++ static/shuttle-master/supplier.js
@@ -4,18 +4,18 @@ $(function () {
   $('ul.tabs').tabs();
 
   // Masonry
-  $('.grid').masonry({
-      itemSelector: '.grid-item'
-  });
+  // $('.grid').masonry({
+  //     itemSelector: '.grid-item'
+  // });
 
   // Reinitialize masonry inside each panel after the relative tab link is clicked -
   $('.tab a').on('click', function() {
     // do async to allow menu to open
-    setTimeout( function() {
-       $('.grid').masonry({
-      itemSelector: '.grid-item'
-       }, 1);
-    });
+    // setTimeout( function() {
+    //    $('.grid').masonry({
+    //   itemSelector: '.grid-item'
+    // }, 1);
+    // });
   });
 
 
